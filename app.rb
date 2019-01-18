d1a911cd768de4db83ac5e437796bf7306a6c26a
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-18 13:34:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is David Swarbrick"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Austin, TX"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Kings & Queens by Thrity Seconds to Mars"
+  end
+  
 end
