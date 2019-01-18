bfb9431dc1dcff17e19cbe273293202fa40af743
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2019-01-18 13:34:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
   
   get '/favorite-song' do 
-    "My favorite song is Kings & Queens by "
+    "My favorite song is Kings & Queens by Thrity Seconds to Mars"
   end
   
 end
